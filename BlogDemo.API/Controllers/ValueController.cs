5d42a20eccae6bef09023b03d3769f06fa9aa08b
raphaelli <raphael_li@live.com>
2018-10-09 14:07:38 +0800

    init & fist run

---
+++ BlogDemo.API/Controllers/ValueController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogDemo.API.Controllers
+{
+    [Route("api/values")]
+    public class ValueController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+        }
+
+
+    }
+}
--- BlogDemo.API/Program.cs
+++ BlogDemo.API/Program.cs
@@ -20,5 +20,7 @@ namespace BlogDemo.API
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+
     }
 }
